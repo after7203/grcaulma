d304b055e891d5b40266167befd98947c851c069
Seoung Oh <after7203@naver.com>
2023-05-18 04:59:31 +0900

    test

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -18,6 +18,7 @@ dotenv.config();
     .catch((err) => {
       console.log(err);
     }))();
+// mongoose.connect(process.env.MONGO_URI as string, {dbName: "grcaulma",}).then(() => {console.log("MongoDB Connected");}).catch((err) => {console.log(err);})
 
 const typeDefs = gql`
   type Query {
@@ -36,7 +37,19 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     products() {
-      return Products.find().then((products) => products);
+      //return Products.find().then((products) => products);
+      return [
+        {
+          __typename: "Product",
+          _id: "64115659aabbe3224593ba82",
+          name: "마이크로닉스 Master M60 메쉬",
+          imgSrc:
+            "//img.danawa.com/prod_img/500000/167/423/img/10423167_1.jpg?shrink=130:130&_v=20221205130722",
+          type: null,
+          price: 44540,
+          category: "케이스",
+        },
+      ];
     },
   },
 };
